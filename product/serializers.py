4b186e1a2bd2d4f428d8de1b995b59b3e07c3d3b
Danny Nasra <danina964@gmail.com>
2023-03-06 22:57:16 +0100

    add a new columen from serlializer

--- product/serializers.py
+++ product/serializers.py
@@ -10,9 +10,13 @@ class BrandSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    brand =BrandSerializer() #the brand cloumen in class Product
+    brand =BrandSerializer() #the brand cloumen in class Produc
+    price_with_tax = serializers.SerializerMethodField(method_name='myfunc')
 
     class Meta:
         model = Product
         exclude = []
         fileds = '__all__'
+
+    def myfunc(self,Product):
+        return Product.price*1.1
