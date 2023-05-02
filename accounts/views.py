8b07a1301d843bc476a75dd4aafbaaa017f56395
Danny Nasra <danina964@gmail.com>
2023-05-02 21:16:48 +0200

    add dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render ,redirect
 from .models import Profile,Address,ContactNumbers
 from .forms import Activate,SignUpForm
 from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from product.models import Product,Brand,Reviews
+from orders.models import Order
 # Create your views here.
 
 def signUp(request):
@@ -49,5 +52,26 @@ def profile(request):
     contactNumbers=ContactNumbers.objects.filter(user=request.user)
     return render(request,'accounts/profile.html',{'profile':profile,'contactNumbers':contactNumbers,'address':address})
 
-def dashboard():
-    pass
+def dashboard(request):
+    user =User.objects.all().count()
+    product =Product.objects.all().count()
+    reviews =Reviews.objects.all().count()
+    brands = Brand.objects.all().count()
+    orders = Order.objects.all().count()
+
+    recieved = Order.objects.filter(orderStatus='Recieved').count()
+    processed = Order.objects.filter(orderStatus='Processed').count()
+    delivered = Order.objects.filter(orderStatus='Delivered').count()
+    shipped = Order.objects.filter(orderStatus='Shipped').count()
+
+    return render(request,'accounts\dashboard.html',{
+        'user':user,
+        'product':product,
+        'reviews':reviews,
+        'brands':brands,
+        'orders':orders,
+        'recieved':recieved,
+        'processed':processed,
+        'delivered':delivered,
+        'shipped':shipped,
+    })
