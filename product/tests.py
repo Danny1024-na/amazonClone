5ccaf8bcbba2dbf78ef25f496120df7db19f229d
Danny Nasra <danina964@gmail.com>
2023-05-21 09:17:56 +0200

    21.05.23

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+def test_product_list_view(self):
+    response =self.client.get('/amazonclone')
+    self.assertEqual(response.status_code,200)
+
+def text_views_uses_correct_template(self):
+    response = self.client.get('/amazonclone')
+    self.assertTemplateUsed(response ,'product_list.html')
+
